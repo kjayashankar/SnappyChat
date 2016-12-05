4d44d3c166590e4cf52d4d60a904a501aafe9826
jay <kjayashankar@yahoo.com>
2016-12-04 16:58:00 -0800

    Search friend v1.0 -- integrating saik work

---
+++ app/src/main/java/com/sjsu/snappychat/Friend.java
@@ -0,0 +1,17 @@
+package com.sjsu.snappychat;
+
+/**
+ * Created by jay on 12/4/16.
+ */
+
+public class Friend {
+    String image;
+    String name;
+    String level;
+
+    Friend(String name, String level) {
+        this.image = image;
+        this.name = name;
+        this.level = level;
+    }
+}
